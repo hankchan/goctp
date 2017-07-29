0d862b3e3a6510c08a09991d09bbe1921e4fb127
Hank Chan <hankchan@qq.com>
2017-07-29 23:32:08 +0800

    参考Femas的API目录，将CTP API 目录结构重构

--- libgoctp.go
+++ libgoctp.go
@@ -6,8 +6,8 @@
 
 package goctp
 
-// #cgo linux LDFLAGS: -fPIC -L${SRCDIR}/api/ThostTraderApi_v6.3.6_20160606_linux64 -Wl,-rpath=${SRCDIR}/api/ThostTraderApi_v6.3.6_20160606_linux64 -lthostmduserapi -lthosttraderapi -lstdc++
-// #cgo linux CPPFLAGS: -fPIC -I${SRCDIR}/api/ThostTraderApi_v6.3.6_20160606_linux64
-// #cgo windows LDFLAGS: -fPIC -L${SRCDIR}/api/ThostTraderApi_v6.3.6_20160606_win64 -Wl,-rpath=${SRCDIR}/api/ThostTraderApi_v6.3.6_20160606_win64 ${SRCDIR}/api/ThostTraderApi_v6.3.6_20160606_win64/thostmduserapi.lib ${SRCDIR}/api/ThostTraderApi_v6.3.6_20160606_win64/thosttraderapi.lib -lthostmduserapi -lthosttraderapi -lstdc++
-// #cgo windows CPPFLAGS: -fPIC -I${SRCDIR}/api/ThostTraderApi_v6.3.6_20160606_win64
+// #cgo linux LDFLAGS: -fPIC -L${SRCDIR}/api/ThostTraderApi_v6.3.6/linux64 -Wl,-rpath=${SRCDIR}/api/ThostTraderApi_v6.3.6/linux64 -lthostmduserapi -lthosttraderapi -lstdc++
+// #cgo linux CPPFLAGS: -fPIC -I${SRCDIR}/api/ThostTraderApi_v6.3.6/include
+// #cgo windows LDFLAGS: -fPIC -L${SRCDIR}/api/ThostTraderApi_v6.3.6/win64 -Wl,-rpath=${SRCDIR}/api/ThostTraderApi_v6.3.6/win64 ${SRCDIR}/api/ThostTraderApi_v6.3.6/win64/thostmduserapi.lib ${SRCDIR}/api/ThostTraderApi_v6.3.6/win64/thosttraderapi.lib -lthostmduserapi -lthosttraderapi
+// #cgo windows CPPFLAGS: -fPIC -I${SRCDIR}/api/ThostTraderApi_v6.3.6/include -DISLIB -DWIN32 -DLIB_MD_API_EXPORT -DLIB_TRADER_API_EXPORT
 import "C"
